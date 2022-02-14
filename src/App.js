88992672d5817b88d070766c34f34c99ead5134c
Viktoria Mincheva <victoria.mincheva97@gmail.com>
2022-02-14 18:23:26 +0200

    header and some styling added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import './App.css';
-import { BrowserRouter, HashRouter, Link, Route, Routes } from "react-router-dom";
-import ErrorPage from './screens/ErrorPage';
-import RegisterPage from './screens/RegisterPage';
-import MyProfile from './screens/MyProfile';
-import Home from './screens/Home';
-import Header from './components/Header';
-import Inbox from './screens/Inbox';
-import ExplorePage from './screens/ExplorePage';
-import LoginPage from './screens/LoginPage';
-
-
+import "./App.css";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+import ErrorPage from "./screens/ErrorPage";
+import RegisterPage from "./screens/RegisterPage";
+import MyProfile from "./screens/MyProfile";
+import Home from "./screens/Home";
+import Header from "./components/Header";
+import Inbox from "./screens/Inbox";
+import ExplorePage from "./screens/ExplorePage";
+import LoginPage from "./screens/LoginPage";
 
 function App() {
   return (
-    <>
-    <Header />
-    
+    <div className="App">
+      <Header />
+
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/" element={<Home />}/>
-        <Route path="/inbox" element={<Inbox/>}/>
-        <Route path="/explore" element={<ExplorePage/>}/>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/explore" element={<ExplorePage />} />
         <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="*" element={<ErrorPage/>} /> 
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-      </>
-  )
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,69 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import React from "react";
+import styled from "@emotion/styled";
+import "../styles/Header.css";
 
-export default function Header () {
+export default function Header() {
 
-    return (
-        <header>
-            <Link to="/">
-                <img src="ig-logo.png" alt="Instagram" />
-            </Link>
+  let location = useLocation();
+  const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 960px;
+  `
+  const InputContainer = styled.div`
+    background-color: #efefef;
+    padding: 0 16px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    border-radius: 8px;
+  `
+  const SearchInput = styled.input`
+    border: none;
+    background-color: #efefef;
+    font-size: 16px;
+    font-family: inherit;
+    font-weight: 300;
+  `
+  const IconsContainer = styled.div`
+    display: flex;
+    gap: 24px;
+    padding-top: 8px;
+  `
 
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/inbox">Inbox</Link>
-            <Link to="/explore">Explore</Link>
-            <Link to="/my-profile">My profile</Link>
-            
-        </header>
-    )
+  return (
+    <header className="Header">
+      <HeaderContainer>
+        <Link to="/">
+          <img src="ig-logo.png" alt="Instagram" style={{"padding-top":"12px" }}/>
+        </Link>
+
+        <InputContainer>
+          <img src="images/icons/search-gray.png" alt="search" className="input-icon"/>
+          <SearchInput type="text" placeholder="Search"/>
+        </InputContainer>
+
+        <IconsContainer>
+          <Link to="/">
+            <img src={location.pathname === "/" ? "images/icons/home-clicked.png" : "images/icons/home.png" } alt="home" className="header-icon" />
+          </Link>
+          <Link to="/inbox">
+            <img src={location.pathname === "/inbox" ? "images/icons/clicked-inbox.png" : "images/icons/inbox.png"} alt="inbox" className="header-icon" />
+          </Link>
+          <img src="images/icons/create.png" alt="create" className="header-icon" />
+          <Link to="/explore">
+            <img src={location.pathname === "/explore" ? "images/icons/explore-clicked.png" : "images/icons/explore.png"} alt="explore" className="header-icon" />
+          </Link>
+          <img src="images/icons/heart.png" alt="notifications" className="header-icon"/>
+          <Link to="/my-profile">
+            <img src="images/icons/profile.png" alt="profile pic" className="header-icon"/>
+          </Link>
+        </IconsContainer>
+      </HeaderContainer>
+    </header>
+  );
 }

f96e6bd3f517de0a2ea969c56cf83efc2cce4269
Viktoria Mincheva <victoria.mincheva97@gmail.com>
2022-02-14 22:23:45 +0200

    Add create post-modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import React from "react";
 import styled from "@emotion/styled";
 import "../styles/Header.css";
+import Modal from "./Modal.js";
 
 export default function Header() {
 
@@ -34,12 +36,18 @@ export default function Header() {
     gap: 24px;
     padding-top: 8px;
   `
+  const [show, setShow] = useState(false);
+
+  const handleCreate = (e) => {
+    e.target.src = "images/icons/create-clicked.png";
+    setShow(true);
+  }
 
   return (
     <header className="Header">
       <HeaderContainer>
         <Link to="/">
-          <img src="ig-logo.png" alt="Instagram" style={{"padding-top":"12px" }}/>
+          <img src="ig-logo.png" alt="Instagram" style={{"paddingTop":"12px" }}/>
         </Link>
 
         <InputContainer>
@@ -54,7 +62,14 @@ export default function Header() {
           <Link to="/inbox">
             <img src={location.pathname === "/inbox" ? "images/icons/clicked-inbox.png" : "images/icons/inbox.png"} alt="inbox" className="header-icon" />
           </Link>
-          <img src="images/icons/create.png" alt="create" className="header-icon" />
+
+          <img src="images/icons/create.png" alt="create" className="header-icon" onClick={(e) => handleCreate(e)}/>
+          <Modal title="Create new post" onClose={() => setShow(false)} show={show}>
+            <img src="create-add.png" alt="add"/>
+            <p>Drag photos and videos here</p>
+            <button type="button">Select From Computer</button>  
+          </Modal>
+
           <Link to="/explore">
             <img src={location.pathname === "/explore" ? "images/icons/explore-clicked.png" : "images/icons/explore.png"} alt="explore" className="header-icon" />
           </Link>

6b467ae1fb080fcda837b74b7f8421835a4254b5
Viktoria Mincheva <victoria.mincheva97@gmail.com>
2022-02-15 19:14:46 +0200

    Fixed Header.js styles and Header.css -> Header.module.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,68 +2,40 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import React from "react";
-import styled from "@emotion/styled";
-import "../styles/Header.css";
+import styles from "../styles/Header.module.css";
 import Modal from "./Modal.js";
+import { style } from "@mui/system";
 
 export default function Header() {
 
   let location = useLocation();
-  const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    max-width: 960px;
-  `
-  const InputContainer = styled.div`
-    background-color: #efefef;
-    padding: 0 16px;
-    height: 36px;
-    display: flex;
-    align-items: center;
-    border-radius: 8px;
-  `
-  const SearchInput = styled.input`
-    border: none;
-    background-color: #efefef;
-    font-size: 16px;
-    font-family: inherit;
-    font-weight: 300;
-  `
-  const IconsContainer = styled.div`
-    display: flex;
-    gap: 24px;
-    padding-top: 8px;
-  `
   const [show, setShow] = useState(false);
 
   const handleCreate = (e) => {
-    e.target.src = "images/icons/create-clicked.png";
     setShow(true);
   }
 
   return (
-    <header className="Header">
-      <HeaderContainer>
+    <header className={styles.Header}>
+      <div className={styles.HeaderContainer}>
         <Link to="/">
           <img src="ig-logo.png" alt="Instagram" style={{"paddingTop":"12px" }}/>
         </Link>
 
-        <InputContainer>
-          <img src="images/icons/search-gray.png" alt="search" className="input-icon"/>
-          <SearchInput type="text" placeholder="Search"/>
-        </InputContainer>
+        <div className={styles.InputContainer}>
+          <img src="images/icons/search-gray.png" alt="search" className={styles.inputIcon}/>
+          <input type="text" placeholder="Search" className={styles.SearchInput}/>
+        </div>
 
-        <IconsContainer>
+        <div className={styles.IconsContainer}>
           <Link to="/">
-            <img src={location.pathname === "/" ? "images/icons/home-clicked.png" : "images/icons/home.png" } alt="home" className="header-icon" />
+            <img src={location.pathname === "/" ? "images/icons/home-clicked.png" : "images/icons/home.png"} alt="home" className={styles.headerIcon} />
           </Link>
           <Link to="/inbox">
-            <img src={location.pathname === "/inbox" ? "images/icons/clicked-inbox.png" : "images/icons/inbox.png"} alt="inbox" className="header-icon" />
+            <img src={location.pathname === "/inbox" ? "images/icons/clicked-inbox.png" : "images/icons/inbox.png"} alt="inbox" className={styles.headerIcon} />
           </Link>
 
-          <img src="images/icons/create.png" alt="create" className="header-icon" onClick={(e) => handleCreate(e)}/>
+          <img src={show ? "images/icons/create-clicked.png" : "images/icons/create.png"} alt="create" className={styles.headerIcon} onClick={(e) => handleCreate(e)}/>
           <Modal title="Create new post" onClose={() => setShow(false)} show={show}>
             <img src="create-add.png" alt="add"/>
             <p>Drag photos and videos here</p>
@@ -71,14 +43,14 @@ export default function Header() {
           </Modal>
 
           <Link to="/explore">
-            <img src={location.pathname === "/explore" ? "images/icons/explore-clicked.png" : "images/icons/explore.png"} alt="explore" className="header-icon" />
+            <img src={location.pathname === "/explore" ? "images/icons/explore-clicked.png" : "images/icons/explore.png"} alt="explore" className={styles.headerIcon} />
           </Link>
-          <img src="images/icons/heart.png" alt="notifications" className="header-icon"/>
+          <img src="images/icons/heart.png" alt="notifications" className={styles.headerIcon}/>
           <Link to="/my-profile">
-            <img src="images/icons/profile.png" alt="profile pic" className="header-icon"/>
+            <img src="images/icons/profile.png" alt="profile pic" className={styles.headerIcon}/>
           </Link>
-        </IconsContainer>
-      </HeaderContainer>
+        </div>
+      </div>
     </header>
   );
 }

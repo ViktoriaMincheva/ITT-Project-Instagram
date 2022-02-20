bb5630aa4c1eff3d52b604e405828bac9530ce03
Aleksandra <ateshovska@gmail.com>
2022-02-20 10:41:11 +0200

    react redux installed

--- src/components/HomeAsideHeader.js
+++ src/components/HomeAsideHeader.js
@@ -1,9 +1,10 @@
 import styles from "../styles/home-aside-header.module.css"
-import {useDispatch} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { logoutAction } from '../redux/actions/userActions';
 import { useAuth } from './../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+
 
 
 export default function HomeAsideHeader(props) {
@@ -12,6 +13,7 @@ export default function HomeAsideHeader(props) {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const logout = useAuth();
+    const userEmail = useSelector(state => state.userData.id);
 
     async function handleLogout() {
         setError("");
@@ -30,7 +32,7 @@ export default function HomeAsideHeader(props) {
             <div className={styles.left}>
                 <a href="#"><img className={styles.icon} src={props.icon} alt="avatar"/></a>
                 <div className={styles.userInfo}>
-                    <a href="#" className={styles.username}>{props.username}<br/><span className={styles.name}>{props.name}</span></a>
+                    <a href="#" className={styles.username}>{userEmail}<br/><span className={styles.name}>{props.name}</span></a>
                 </div>
             </div>
             <Link to="/login"><button className={styles.btn} onClick={handleLogout}>Log out</button></Link>
